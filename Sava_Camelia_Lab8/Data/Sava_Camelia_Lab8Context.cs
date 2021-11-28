7906d068f7011e9688b07644c760ec0d907fd24b
Sava Camelia <camelia_cristina00@yahoo.com>
2021-11-28 21:28:10 +0200

    Laborator8
    rezolvarea laboratorului 8

---
+++ Sava_Camelia_Lab8/Data/Sava_Camelia_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sava_Camelia_Lab8.Models;
+
+namespace Sava_Camelia_Lab8.Data
+{
+    public class Sava_Camelia_Lab8Context : DbContext
+    {
+        public Sava_Camelia_Lab8Context (DbContextOptions<Sava_Camelia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sava_Camelia_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Sava_Camelia_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Sava_Camelia_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Sava_Camelia_Lab8/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sava_Camelia_Lab8.Data;
+using Sava_Camelia_Lab8.Models;
+
+namespace Sava_Camelia_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sava_Camelia_Lab8.Data.Sava_Camelia_Lab8Context _context;
+
+        public IndexModel(Sava_Camelia_Lab8.Data.Sava_Camelia_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
